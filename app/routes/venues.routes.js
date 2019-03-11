ae1019707b67d440a4d87e17f5b150175cbb6aae
dalyIsaac <isaac.daly@outlook.com>
2019-03-12 09:52:17 +1300

    Working

--- app/routes/venues.routes.js
+++ app/routes/venues.routes.js
@@ -4,4 +4,5 @@ module.exports = app => {
   app.route("/api/v1/venues").get(venues.get);
   app.route("/api/v1/venues/:id").get(venues.getSingle);
   app.route("/api/v1/venues/").post(venues.create);
+  app.route("/api/v1/venues/:id").patch(venues.patch);
 };
