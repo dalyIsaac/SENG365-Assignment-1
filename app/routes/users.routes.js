d941cecb2ac77603e41975859846fe55f54a651e
dalyIsaac <isaac.daly@outlook.com>
2019-03-14 12:27:33 +1300

    Refactored routes

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -2,12 +2,16 @@ const users = require("../controllers/users/users.controller");
 const photos = require("../controllers/users/photos.controller");
 
 module.exports = app => {
-  app.route("/api/v1/users").post(users.create);
-  app.route("/api/v1/users/login").post(users.login);
-  app.route("/api/v1/users/logout").post(users.logout);
-  app.route("/api/v1/users/:id").get(users.getUser);
-  app.route("/api/v1/users/:id").patch(users.updateUser);
-  app.route("/api/v1/users/:id/photo").put(photos.upload);
-  app.route("/api/v1/users/:id/photo").get(photos.getPhoto);
-  app.route("/api/v1/users/:id/photo").delete(photos.deletePhoto);
+  app.route(app.rootUrl + "/users").post(users.create);
+  app.route(app.rootUrl + "/users/login").post(users.login);
+  app.route(app.rootUrl + "/users/logout").post(users.logout);
+  app
+    .route(app.rootUrl + "/users/:id")
+    .get(users.getUser)
+    .patch(users.updateUser);
+  app
+    .route(app.rootUrl + "/users/:id/photo")
+    .put(photos.upload)
+    .get(photos.getPhoto)
+    .delete(photos.deletePhoto);
 };
--- app/routes/venues.routes.js
+++ app/routes/venues.routes.js
@@ -5,12 +5,16 @@ const multer = require("multer");
 const upload = multer({ dest: "media/tempVenues/" });
 
 module.exports = app => {
-  app.route("/api/v1/venues").get(venues.get);
-  app.route("/api/v1/venues/:id").get(venues.getSingle);
-  app.route("/api/v1/venues/").post(venues.create);
-  app.route("/api/v1/venues/:id").patch(venues.patch);
-  app.route("/api/v1/categories/").get(venues.getCategories);
   app
-    .route("/api/v1/venues/:id/photos")
+    .route(app.rootUrl + "/venues")
+    .get(venues.get)
+    .post(venues.create);
+  app
+    .route(app.rootUrl + "/venues/:id")
+    .get(venues.getSingle)
+    .patch(venues.patch);
+  app.route(app.rootUrl + "/categories/").get(venues.getCategories);
+  app
+    .route(app.rootUrl + "/venues/:id/photos")
     .post(upload.fields([{ name: "photo", maxCount: 1 }]), photos.upload);
 };
